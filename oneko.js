75530d663ee2ad26ad13b67d23d67234caf8b61b
rikachu225 <86760005+rikachu225@users.noreply.github.com>
2024-10-03 23:06:00 -0400

    Update oneko.js

--- oneko.js
+++ oneko.js
@@ -96,7 +96,7 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
     nekoEl.style.zIndex = 2147483647;
 
-    let nekoFile = "./oneko.gif"
+    let nekoFile = "https://rikachu225.github.io/oneko-host/oneko.gif"
     const curScript = document.currentScript
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat
